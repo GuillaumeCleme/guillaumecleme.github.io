4bb50407056dbf427109c7b61536f8ce68349c78
Guillaume Clement <guillaume.clement@aftia.com>
2019-06-13 17:56:24 -0400

    Issue #10 replace navbar click event with scroll event listener that will update analytics page_views on section scroll (manual or via click).

--- js/resume.js
+++ js/resume.js
@@ -7,26 +7,48 @@
   //Set document title
   window.resume.title = $(document).find("title").text();
 
+  //Set document sections
+  window.resume.sections = $("section");
+
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
 
-      //Log page_view event
-      gtag('event', 'page_view', {
-        'page_title': window.resume.title,
-        'page_path' : "/" + this.hash
-      });
-
       //Animate scroll
       if (target.length) {
         $('html, body').animate({
           scrollTop: (target.offset().top)
         }, 1000, "easeInOutExpo");
+        return false;
       }
     }
   });
+
+  //Listen for scrolling and update visited section
+  $(window).scroll(
+    function(){
+      var currentScroll = $(this).scrollTop();
+      //Loop through section to find if we've scrolled through it
+      $.each(window.resume.sections.get().reverse(), function(index, object){
+        if(currentScroll > object.offsetTop-50){
+          object = $(object);
+          //Update the current section if we haven't visited it before
+          if(object.attr("visited") !== "true"){
+            object.attr("visited", "true");
+            //Log page_view event
+            gtag('event', 'page_view', {
+              'page_title': window.resume.title,
+              'page_path' : "/#" + object.attr("id")
+            });
+          }
+          //Stop loop
+          return false;
+        }
+      });
+    }
+  );
 
   // Closes responsive menu when a scroll trigger link is clicked
   $('.js-scroll-trigger').click(function() {
