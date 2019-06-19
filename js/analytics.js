686943a98a030009543692735c2e3d632a698b5f
Guillaume Clement <guillaume.clement@aftia.com>
2019-06-19 11:43:05 -0400

    Issue #10 moving script to individual files, using concat script

---
+++ js/analytics.js
@@ -0,0 +1,10 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  //Define namespace if not exist
+  window.resume = window.resume || {};
+  window.resume.analytics = window.resume.analytics || {};
+
+
+
+})(jQuery); // End of use strict
--- js/resume.js
+++ js/resume.js
@@ -4,17 +4,9 @@
   //Define namespace if not exist
   window.resume = window.resume || {};
 
-  //Set document title
+  //Set document title/sections
   window.resume.title = $(document).find("title").text();
-
-  //Set document sections
   window.resume.sections = $("section");
-
-  //Define new timer to monitor user interaction
-  window.resume.skillTableTimer = null;
-  window.resume.skillTableQuery = null;
-  window.resume.skillTableEventSent = false;
-  window.resume.skillTableLastKeyDelete = false;
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
@@ -64,97 +56,5 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#sideNav'
-  });
-
-  //Bind Datatables
-  $('#skillsTable').DataTable({
-        "ajax": "js/skills.min.json",
-        "dom": '<"form-row bg-gray-100"' +
-               '<"form-group col-md-6 mb-0 pl-0" <"input-group" <"input-group-prepend" <"input-group-text" <"fas fa-search">>> f>>' +
-               '<"form-group col-md-6 mb-0 pl-0 pl-md-1" p>>' +
-               '<t>',
-        "language": {
-          "search": "",
-          "searchPlaceholder": "Search skills"
-        },
-        "pageLength": 14,
-        "info": false,
-        "columnDefs": [
-          {
-            "targets": 0,
-            "width": "40%",
-            "responsivePriority": 3,
-            "className": "d-none d-md-block"
-          },
-          {
-            "targets": 1,
-            "width": "60%",
-            "responsivePriority": 1,
-            "className": "d-md-none"
-          },
-          {
-            "targets": 2,
-            "width": "60%",
-            "responsivePriority": 2,
-            "data": "skill_level",
-            "render": function ( data, type, row, meta ) {
-              return '<div class="progress">'+
-                        '<div class="progress-bar bg-warning" role="progressbar" style="width:' + data + '%" aria-valuenow="' + data + '" aria-valuemin="0" aria-valuemax="100">' + data + '%</div>' +
-                      '</div>';
-            }
-          }
-        ],
-        "columns": [
-            { "data": "skillLong" },
-            { "data": "skillShort" },
-            { "data": "level" }
-        ],
-        "initComplete": function() {
-          $('#skillsTable_filter').find("input").on("input", function(event){
-            //If the event has not been sent
-            if(!window.resume.skillTableEventSent){
-
-              //Set new query value
-              window.resume.skillTableQuery = event.target.value.toString();
-
-              //Clear previous timeout
-              if(window.resume.skillTableTimer != null){
-                clearTimeout(window.resume.skillTableTimer);
-              }
-
-              //Start new timer before sending event
-              window.resume.skillTableTimer = setTimeout(function(){
-                if(window.resume.skillTableQuery.length > 0){
-                  console.log("Sending:" + window.resume.skillTableQuery); //Send event
-                  window.resume.skillTableEventSent = true; //Set event sent
-                }
-              }, 2000); //Wait 2 seconds
-            }
-
-          }).keydown(function(event){
-            //Detect text deletion (Backspace/Delete)
-            if((event.which === 46 || event.which === 8)){
-              if(!window.resume.skillTableEventSent){
-                //Clear previous timeout
-                if(window.resume.skillTableTimer != null){
-                  clearTimeout(window.resume.skillTableTimer);
-                }
-
-                if (window.resume.skillTableQuery.length > 0) {
-                  console.log("Sending:" + window.resume.skillTableQuery);
-                }
-                
-                //Ensure the event does not fire again
-                window.resume.skillTableEventSent = true;
-              }
-              //Set last key to delete
-              window.resume.skillTableLastKeyDelete = true;
-            }
-            else{
-              window.resume.skillTableEventSent = false; //Event not sent
-              window.resume.skillTableLastKeyDelete = false; //Last key not delete
-            }
-          });
-        }
   });
 })(jQuery); // End of use strict
