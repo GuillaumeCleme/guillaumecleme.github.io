38f3ce6bec1475083dc0104e2d451640041b45c3
Guillaume Clement <guillaume.clement@aftia.com>
2020-11-21 12:10:27 -0500

    Issue #37 Migrating first NavBar component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <NavBar />
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -7,11 +8,13 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    NavBar
   }
 }
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <nav class="fixed z-50 w-full bg-white top-0 flex flex-wrap items-center justify-between px-2 py-3 navbar-expand-lg shadow-lg">
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-gray-800"
+          href="#">
+          Guillaume Clement
+        </a>
+        <button
+          class="toggleNav cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
+          type="button">
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+      <div class="lg:flex flex-grow items-center hidden" id="top-navbar">
+        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
+          <li class="nav-item text-gray-800 hover:text-gray-600">
+            <a href="#aboutme"
+              class="toggleNav px-3 py-2 flex items-center text-xs uppercase font-bold">
+              <i class="fas fa-address-card text-lg leading-lg"></i>
+              <span class="ml-2">About Me</span>
+            </a>
+          </li>
+          <li class="nav-item text-gray-800 hover:text-gray-600">
+            <a href="#experience"
+              class="toggleNav px-3 py-2 flex items-center text-xs uppercase font-bold">
+              <i class="fas fa-briefcase text-lg leading-lg"></i>
+              <span class="ml-2">My Experience</span>
+            </a>
+          </li>
+          <li class="nav-item text-gray-800 hover:text-gray-600">
+            <a href="./blog.html"
+              class="toggleNav px-3 py-2 flex items-center text-xs uppercase font-bold">
+              <i class="fas fa-book text-lg leading-lg"></i>
+              <span class="ml-2">My Blog</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
