b89f16dc952795f065161a92dbeb4bfb0b4c6633
Guillaume Clement <guillaume.clement@aftia.com>
2020-11-22 00:00:31 -0500

    Issue #37 Adding FontAwesome support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import '@/assets/css/tailwind.css'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faStackOverflow)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false
 
