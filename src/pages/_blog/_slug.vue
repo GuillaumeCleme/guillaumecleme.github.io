6cebb694f020048faaf8d18898de32ff662aaeac
Guillaume Clement <guillaume.clement@aftia.com>
2021-01-03 15:41:08 -0500

    Issue #39 Fixing Blog slug separator

--- src/pages/_blog/_slug.vue
+++ src/pages/_blog/_slug.vue
@@ -7,7 +7,22 @@
         <CoverImage
           :cover="post.cover"
         />
-        <SlopeSeparator direction="desc" fill-color="text-gray-300" />
+        <div
+          class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden"
+          style="height: 70px; transform: translateZ(0px);"
+        >
+          <svg
+            class="absolute bottom-0 overflow-hidden"
+            xmlns="http://www.w3.org/2000/svg"
+            preserveAspectRatio="none"
+            version="1.1"
+            viewBox="0 0 2560 100"
+            x="0"
+            y="0"
+          >
+            <polygon class="text-gray-300 fill-current" points="2560 0 2560 100 0 100" />
+          </svg>
+        </div>
       </section>
       <section class="relative py-24 bg-gray-300">
         <div class="container mx-auto px-4">
@@ -35,7 +50,6 @@
 
 <script>
 import NavBar from '../../components/NavBar.vue'
-import SlopeSeparator from '../../components/SlopeSeparator.vue'
 import MainFooter from '../../components/MainFooter.vue'
 import ProfileHeader from '../../components/ProfileHeader.vue'
 import ProfileImage from '../../components/ProfileImage.vue'
@@ -44,7 +58,6 @@ export default {
   name: 'BlogPost',
   components: {
     NavBar,
-    SlopeSeparator,
     MainFooter,
     ProfileHeader,
     ProfileImage
