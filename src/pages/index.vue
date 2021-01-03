5b508429c01e40ea3637d2b998786699ca1e4eba
Guillaume Clement <guillaume.clement@aftia.com>
2021-01-03 11:55:50 -0500

    Issue #39 Updating default layout styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -31,10 +31,3 @@ export default {
   }
 }
 </script>
-
-<style>
-html {
-    scroll-behavior: smooth;
-    font-family: system-ui,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
-}
-</style>
