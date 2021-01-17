c3ee079bbe7c43b8017b7487d87552287697191e
Guillaume Clement <guillaume.clement@aftia.com>
2021-01-17 13:48:36 -0500

    Experience timeline hotfix

--- src/components/Sections/Fragments/ExperienceTimelineItem.vue
+++ src/components/Sections/Fragments/ExperienceTimelineItem.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="mb-2">
     <div class="flex items-center mb-1">
-      <div class="bg-blue-400 rounded-full h-8 w-8" :class="index == 0 ? 'mt-5 sm:mt-0' : ''" />
+      <div class="bg-blue-400 rounded-full h-8 w-8" :class="index == 0 ? '-mt-5 sm:mt-0' : ''" />
       <div class="flex-1 ml-4 font-medium">
         {{ title }} &#8226;
         <span class="font-light">{{ timeline }}</span>
